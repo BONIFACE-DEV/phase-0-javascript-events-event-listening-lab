3e977ac213c09d3045c1ddce76eaeaa82c8e9450
BONIFACE KORIR <boniface.korir@student.moringa.school@gmail.com>
2022-11-20 20:23:11 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-function addingEventListener() {
-}
+function addingEventListener(){
+  document.querySelector("#button").addEventListener("click",function(event)
+  {
+    console.log("i was clicked")
+  })};
